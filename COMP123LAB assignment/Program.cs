7c8eda023b21d50a3d2a40d3d49c288f93fe7e90
Arshdeep Singh <arshdeepsingh5656@gmail.com>
2020-06-19 18:12:15 -0400

    Add project files.

---
+++ COMP123LAB assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123LAB_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Aditi = new Person(name: "Aditi Kapoor", age: 19);
+
+            Aditi.SaysHi();
+
+            Console.ReadLine();
+        }
+    }
+}
